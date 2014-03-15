14f34c406730d54a56b67cf7271bf2698bd027b8
jaeheejung <jaeheejung@wustl.edu>
2014-03-14 23:14:21 -0500

    Revised function and output
    I made the main function calculate different regressions for all
    possible combinations of the covariates and output a matrix of
    coefficients and a vector of R2 values. But when I run the function, I
    get the error that there is no inherited method. I don’t know how to
    solve this problem. It must be coming from the getCoefs() and getR2s()
    functions. In other words, I don’t know how to embed the functions I
    defined in the main function.

--- regressionPack/R/Regressions.R
+++ regressionPack/R/Regressions.R
@@ -12,20 +12,20 @@
 #' }
 #'
 #' @author Jae Hee Jung: \email{jaeheejung@@wustl.edu}
-#' @aliases Regressions-class initialize,Regressions-method getRegressions,Regressions-method 
+#' @aliases Regressions-class initialize,Regressions-method getRegressions,Regressions-method getCoefs,Regressions-method getR2s,Regressions-method 
 #' @rdname Regressions
 #' @export
 setClass(Class="Regressions",
 	representation=representation(
 		x="matrix",
 		y="matrix",
-		coefficients="numeric",
+		coefficients="matrix",
 		Rsquared="numeric"
 		),
 	prototype=prototype(
 		x=matrix(),
 		y=matrix(),
-		coefficients=numeric(),
+		coefficients=matrix(),
 		Rsquared=numeric()
 		)
 	)
@@ -52,3 +52,71 @@ setMethod("getRegressions", "Regressions",
           return(list(coefficients=object@coefficients,Rsquared=object@Rsquared))
         }
         )
+
+#' @rdname Regressions
+#' @export 
+setGeneric("getCoefs",
+     function(x,y,...)  {
+         standardGeneric("getCoefs")
+       }
+       )
+
+#' @export
+setMethod("getCoefs","Regressions",
+     function(x,y,...){
+    x <- data.frame(x)
+    
+    y <- data.frame(y)
+	
+	covariate.names <- colnames(x)
+	
+	outcome.name <- colnames(y)
+	
+	combinations <- unlist(lapply(1:ncol(x),function(X){combn(ncol(x),X,simplify=F)}),recursive=F)
+	
+	formulas <- sapply(combinations,function(X){paste(paste(outcome.name,"~"),paste(covariate.names[X],collapse="+"))})
+	
+	all.lm <- lapply(formulas,function(X){lm(as.formula(X),data=cbind(y,x))})
+	
+	coef.matrix <- matrix("NA",nrow=length(all.lm),ncol=ncol(a)+1)
+	
+	for(i in 1:nrow(empty)){
+coef.matrix[i,seq(length(unlist(sapply(all.lm,coef)[i],use.names=FALSE)))]<- unlist(sapply(all.lm,coef)[i],use.names=FALSE)
+	}
+	
+	return(coef.matrix)
+	
+	} 
+    )
+    
+#' @rdname Regressions
+#' @export 
+setGeneric("getR2s",
+     function(x,y,...)  {
+         standardGeneric("getR2s")
+       }
+       )
+
+#' @export
+setMethod("getR2s","Regressions",
+     function(x,y,...){
+    x <- data.frame(x)
+    
+    y <- data.frame(y)
+	
+	covariate.names <- colnames(x)
+	
+	outcome.name <- colnames(y)
+	
+	combinations <- unlist(lapply(1:ncol(x),function(X){combn(ncol(x),X,simplify=F)}),recursive=F)
+	
+	formulas <- sapply(combinations,function(X){paste(paste(outcome.name,"~"),paste(covariate.names[X],collapse="+"))})
+	
+	all.lm <- lapply(formulas,function(X){lm(as.formula(X),data=cbind(y,x))})
+	
+	R2.vector <- sapply(all.lm,function(X){summary(X)$r.squared})
+	
+	return(R2.vector)
+	
+	}
+	)
